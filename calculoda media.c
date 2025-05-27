095e9db3163610e8b834d1cacfeed8399785b00c
Wcesar12 <202501587781@alunos.unifanor.edu.br>
2025-05-27 19:29:21 +0000

    calculo da media

---
+++ calculoda media.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    int nota1, nota2, nota3;
+    int media;
+
+    printf("*** programa de calculo de Media***\n";
+    
+    printf("Digite a sua primeira nota: \n");
+    scanf("%d", &nota1);
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%d", &nota2);
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%d", &nota3);
+
+     media = (nota1 + nota2 + nota3) / 3 ;
+
+    printf("A meida e: %d", media)
+
+
+
+
+
+
+
+
+return 0;
+
+}
